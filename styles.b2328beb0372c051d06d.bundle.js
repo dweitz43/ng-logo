1454da37a2d06bb24cf3572185fc3de8aab30ee1
dweitz43 <dweitz43@gmail.com>
2016-11-23 18:16:11 +0200

    ng-logo now with aot

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{403:function(n,o){},757:function(n,o,c){n.exports=c(403)}},[757]);
+webpackJsonp([1,2],{352:function(n,o){},674:function(n,o,c){n.exports=c(352)}},[674]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
