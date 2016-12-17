c63174dccf1c7759ba92b0b0c6aa010c9388f281
dweitz43 <dweitz43@gmail.com>
2016-12-17 19:00:30 +0200

    svg better practices

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{352:function(n,o){},674:function(n,o,c){n.exports=c(352)}},[674]);
+webpackJsonp([1,3],{350:function(n,o){},676:function(n,o,c){n.exports=c(350)}},[676]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
